33cd8a3a619d075f3210bc4122158d4201275df5
“jungmisu”  <jungmisu801@gmail.com>
2018-12-24 15:47:44 +0900

    문자열 다루기 기본, 문자열 내 p와 y의 개수

---
+++ src/main/java/Collatz.java
@@ -0,0 +1,20 @@
+public class Collatz {
+    public int solution(int num) {
+        int answer = 0;
+        long long_num = num;
+
+        while(answer <= 500){
+            if(long_num == 1){
+                return answer;
+            }else if(long_num % 2 == 0){
+                long_num  = long_num / 2;
+                answer ++;
+            }else{
+                long_num = long_num * 3;
+                long_num ++;
+                answer ++;
+            }
+        }
+        return -1;
+    }
+}
